fefcca612dded3a3bcff8740065fdec473639889
monwaraamoni <158725529+monwaraamoni@users.noreply.github.com>
2024-11-24 11:32:56 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Sarah Parker. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Monwara Moni. All rights reserved.</p>
         </div>
       </div>
     </footer>
